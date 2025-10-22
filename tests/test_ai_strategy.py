360f62b7071211cfae056d4942aed5dd31f16d07
Bryan Grigorie <bryangrigorie@gmail.com>
2025-10-22 16:34:13 -0400

    init ai strategy tests

---
+++ tests/test_ai_strategy.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+from battleship.ai_strategy import RandomStrategy
+from battleship.models import Board, ShotStatus
+
+
+class RandomStrategyTestCase(TestCase):
+    def setUp(self):
+        self.board = Board(size=10)
+        self.strategy = RandomStrategy()
+
+    def test_get_next_shot(self):
+        coord1 = self.strategy.get_next_shot(self.board.size, self.board.get_status_grid())
+        self.assertTrue(coord1.is_valid())
+        self.assertTrue(self.board.get_status_at(coord1) == ShotStatus.UNSHOT)
